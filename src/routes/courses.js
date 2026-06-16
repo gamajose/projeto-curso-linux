78af187f5b9dfee1b72066716fd64447a928a047
jose luiz gama <115404896+gamajose@users.noreply.github.com>
2026-06-16 14:19:06 -0300

    feat: registra rotas de comentários por aula

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const courseController = require('../controllers/courseController');
 const authenticateToken = require('../middleware/auth');
 const manageRoutes = require('./adminContent');
+const lessonCommentsRoutes = require('./lessonComments');
 
 router.get('/', authenticateToken, courseController.getAllCourses);
 router.use('/manage', manageRoutes);
+router.use('/comments', lessonCommentsRoutes);
 
 module.exports = router;
