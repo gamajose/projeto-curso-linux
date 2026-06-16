f8db101c49c0fb77091999a24f8061b79c8321f4
jose luiz gama <115404896+gamajose@users.noreply.github.com>
2026-06-16 14:01:11 -0300

    feat: registra rotas de gerenciamento de cursos

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/courseController');
 const authenticateToken = require('../middleware/auth');
+const manageRoutes = require('./adminContent');
 
 router.get('/', authenticateToken, courseController.getAllCourses);
+router.use('/manage', manageRoutes);
 
 module.exports = router;
